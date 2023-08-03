2c6c997ffb8dbfb856f462a97a07f93378583a71
Felipe Soares <felipe.11.11@hotmail.com>
2023-08-03 14:57:15 -0400

    feat: create logo and tagHeader

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -2,9 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <h1>teste</h1>
-        </main>
-    );
+    return <main className={styles.main}></main>;
 }
---
+++ capputeeno/src/components/Header.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { styled } from "styled-components";
+
+interface HeaderProps {}
+
+import { Saira_Stencil_One } from "next/font/google";
+
+const sairaStencil = Saira_Stencil_One({
+    weight: ["400"],
+    subsets: ["latin"],
+});
+
+const TagHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 160px;
+`;
+
+const Logo = styled.a`
+    color: var(--logo-color);
+    font-size: 40px;
+    line-height: 150%;
+`;
+
+export default function Header(props: HeaderProps) {
+    return (
+        <TagHeader>
+            <Logo className={sairaStencil.className}>capputeeno</Logo>
+            <div></div>
+        </TagHeader>
+    );
+}
