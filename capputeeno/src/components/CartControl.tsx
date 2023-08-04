46f9f9066de455106f4e2a6d854c57a89ae77c1f
Felipe Soares <felipe.11.11@hotmail.com>
2023-08-04 09:52:47 -0400

    feat(header): add CartCount

--- capputeeno/src/components/CartControl.tsx
+++ capputeeno/src/components/CartControl.tsx
@@ -1,13 +1,36 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import ShoppingBagIcon from "./ShoppingBagIcon";
+import { styled } from "styled-components";
+
+const CartCount = styled.span`
+    background-color: var(--delete-color);
+    color: #fff;
+
+    font-size: 10px;
+    font-weight: 500;
+
+    width: 17px;
+    height: 17px;
+    border-radius: 100%;
+    line-height: 17px;
+    text-align: center;
+
+    position: absolute;
+    left: 15px;
+    top: 50%;
+`;
+
+const Container = styled.div`
+    position: relative;
+`;
 
 interface CartControlProps {}
 export default function CartControl(props: CartControlProps) {
     const { value } = useLocalStorage("cart-items");
     return (
-        <div>
+        <Container>
             <ShoppingBagIcon />
-            {value}
-        </div>
+            <CartCount>{value.length}</CartCount>
+        </Container>
     );
 }
